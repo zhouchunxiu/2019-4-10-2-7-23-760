39b04842e624c2b51f7e086441a104275b79f82e
zhouchunxiu <chunxiu_z@163.com>
2019-05-27 13:48:47 +0800

    done version 1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,32 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var dis = inputs['distance'];
+    var time = inputs['parkTime'];
+    var price = calculate(dis, time);
+    return Math.round(price);
 };
+
+function calculate(dis, time) {
+    return caldis(dis) + caltime(time);
+};
+
+function caldis(dis) {
+    dis = Math.ceil(dis);
+    if(dis <= 2){
+        return 6;
+    }else if(dis > 2 && dis <= 8){
+        return 6 + (dis - 2) * 0.8;
+    }else{
+        return 6 + (dis - 2) * 0.8 + (dis - 8) * 1.2;
+    }
+};
+
+function caltime(time) {
+    time = Math.ceil(time);
+    if(time <= 0){
+        return 0;
+    }else{
+        return time * 0.25;
+    }
+};
+
